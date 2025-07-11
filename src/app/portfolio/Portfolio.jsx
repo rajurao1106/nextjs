fce19c8fab76bd6972edb924c121d4657d674641
D Raju Rao <108294345+rajurao1106@users.noreply.github.com>
2025-07-11 11:56:56 +0530

    Update Portfolio.jsx

--- src/app/portfolio/Portfolio.jsx
+++ src/app/portfolio/Portfolio.jsx
@@ -4,6 +4,11 @@ import Link from "next/link";
 import Image from "next/image";
 import { useTheme } from "../../components/theme-context";
 
+// ✅ Import images
+import image1 from "../../images/portfolio/image1.png";
+import image2 from "../../images/portfolio/image2.png";
+import image3 from "../../images/portfolio/image3.png";
+
 const WorkSection = () => {
   const { theme } = useTheme();
 
@@ -13,7 +18,7 @@ const WorkSection = () => {
       category: "Web Design",
       bgColor: "bg-blue-800",
       textColor: "text-white",
-      image: "../..../../images/portfolio/image1.png", // Correct path (from public folder)
+      image: image1,
       size: "col-span-2 row-span-2",
       link: "https://tallyzone.in/",
     },
@@ -22,7 +27,7 @@ const WorkSection = () => {
       category: "Branding",
       bgColor: "bg-blue-500",
       textColor: "text-white",
-      image: "../../images/portfolio/image2.png",
+      image: image2,
       size: "col-span-1 row-span-2",
       link: "https://origenpharmaceutical.com/",
     },
@@ -31,7 +36,7 @@ const WorkSection = () => {
       category: "UI/UX",
       bgColor: "bg-green-500",
       textColor: "text-black",
-      image: "../../images/portfolio/image3.png",
+      image: image3,
       size: "col-span-2 row-span-1",
       link: "https://origenpharmaceutical.com/",
     },
@@ -40,7 +45,7 @@ const WorkSection = () => {
       category: "Web Design",
       bgColor: "bg-green-500",
       textColor: "text-black",
-      image: "../../images/portfolio/image3.png",
+      image: image3,
       size: "col-span-2 row-span-1",
       link: "https://origenpharmaceutical.com/",
     },
@@ -52,13 +57,14 @@ const WorkSection = () => {
         theme ? "bg-gray-900" : "bg-gray-100"
       }`}
     >
-      <div className="container mx-auto px-4 max-w-[1200px]">
+      <div className="mx-auto px-4 max-w-[1200px]">
         <div className="flex justify-center items-center gap-10 flex-col">
           {projects.map((project, index) => (
             <div
               key={index}
               className={`max-lg:w-full w-[60rem] rounded-lg shadow-lg overflow-hidden ${project.bgColor} ${project.size} flex flex-col justify-between p-6`}
             >
+              {/* Text Content */}
               <div>
                 <h3 className={`text-2xl font-semibold ${project.textColor}`}>
                   {project.title}
@@ -68,18 +74,20 @@ const WorkSection = () => {
                 </p>
               </div>
 
-              <Link href={project.link} className="mt-4">
+              {/* Image */}
+              <Link href={project.link} className="mt-4 block">
                 <Image
                   src={project.image}
                   alt={project.title}
                   width={1000}
-                  height={1000}
-                  className="max-lg:w-full w-[60rem] object-cover rounded-md"
+                  height={600}
+                  className="w-full object-cover rounded-md"
                 />
               </Link>
 
+              {/* View More Link */}
               <Link
-                className={`mt-4 inline-block hover:underline text-white`}
+                className={`mt-4 inline-block hover:underline ${project.textColor}`}
                 href={project.link}
               >
                 View More →
